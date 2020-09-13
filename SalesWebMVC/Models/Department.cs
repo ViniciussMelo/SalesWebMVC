c18482f5335839eda7e0a70e8d0945e3b6a21129
Vinicius Spada Melo <vinicius.smelo54@gmail.com>
2020-09-13 10:29:14 -0300

    Add: Other entities and second migration

--- SalesWebMVC/Models/Department.cs
+++ SalesWebMVC/Models/Department.cs
@@ -1,8 +1,40 @@
-﻿namespace SalesWebMVC.Models
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMVC.Models
 {
     public class Department
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        #region CTOR
+        public Department()
+        {
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+        #endregion
+
+        #region AddSeller
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+        #endregion
+
+        #region TotalSales
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
+        #endregion
     }
 }
---
+++ SalesWebMVC/Models/Seller.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMVC.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string email { get; set; }
+        public DateTime BithDate { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+        #region CTOR
+        public Seller()
+        {
+        }
+
+        public Seller(int id, string name, string email, DateTime bithDate, double baseSalary, Department department)
+        {
+            Id = id;
+            Name = name;
+            this.email = email;
+            BithDate = bithDate;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+        #endregion
+
+        #region AddSales
+        public void AddSales(SalesRecord sr)
+        {
+            Sales.Add(sr);
+        }
+        #endregion
+
+        #region RemoveSales
+        public void RemoveSales(SalesRecord sr)
+        {
+            Sales.Remove(sr);
+        }
+        #endregion
+
+        #region TotalSales
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sales.Where(sr => sr.Date >= initial && sr.Date <= final).Sum(sr => sr.Amount);
+        }
+        #endregion
+    }
+}
